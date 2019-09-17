7731b6eede07ba17313cdeca14e52e5484f3eda4
Protik Sarkar <sarkar.protik1@gmail.com>
2019-09-17 20:35:43 +0200

    Updates

---
+++ precache-manifest.f20330442c30ae532b581604b16773c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42af5176aaa81ebabbb9309f395f5ab4",
+    "url": "/simple/index.html"
+  },
+  {
+    "revision": "548223af6de4ba3c73c5",
+    "url": "/simple/static/css/2.6694e725.chunk.css"
+  },
+  {
+    "revision": "66989d01f5f687a397b7",
+    "url": "/simple/static/css/main.e3ed3100.chunk.css"
+  },
+  {
+    "revision": "548223af6de4ba3c73c5",
+    "url": "/simple/static/js/2.51ce1c4b.chunk.js"
+  },
+  {
+    "revision": "66989d01f5f687a397b7",
+    "url": "/simple/static/js/main.8bba0414.chunk.js"
+  },
+  {
+    "revision": "96b9bd60661320be8f2a",
+    "url": "/simple/static/js/runtime~main.251d7b4f.js"
+  },
+  {
+    "revision": "b6d1b5886a63f03de0663ea314c07f03",
+    "url": "/simple/static/media/background.b6d1b588.jpg"
+  },
+  {
+    "revision": "45af7c0f65f783d263065d7aab31fd44",
+    "url": "/simple/static/media/code.45af7c0f.svg"
+  }
+]);
